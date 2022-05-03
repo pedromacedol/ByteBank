8e1c654c2f4fb3fde7f4f055ca50aa586551299f
pedromacedol <pedromacedoluna@gmail.com>
2022-05-03 17:25:26 -0300

    Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
 // ignore: prefer_const_constructors
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: const Text('Transfer')),
-          floatingActionButton: FloatingActionButton(
-              onPressed: () {}, child: const Icon(Icons.add)),
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Card(
+                child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('100.0'),
+              subtitle: Text('1000'),
+            )),
+            Card(
+                child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('200.0'),
+              subtitle: Text('1000'),
+            )),
+          ],
+        ),
+        appBar: AppBar(
+          title: Text('Transfer'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
         ),
       ),
-    );
+    ));
