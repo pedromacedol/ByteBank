81526d58fbcc89fcdb3aca5afd7cfb2b5c6122e1
pedromacedol <pedromacedoluna@gmail.com>
2022-05-03 10:46:55 -0300

    column

--- bytebank/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-// ignore: prefer_const_constructors
-void main() => runApp(Text(
-      'Bem vindo ao Bytebank',
-      textDirection: TextDirection.ltr,
-    ));
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text(
-      'Bem vindo ao Bytebank',
-      textDirection: TextDirection.ltr,
-    ));
+// ignore: prefer_const_constructors
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                'Craft beautiful UIs',
+                textDirection: TextDirection.ltr,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
