eff08bc40920e58fdd0708a3a9786073accf3761
pedromacedol <pedromacedoluna@gmail.com>
2022-05-05 01:37:06 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(bytebank());
+
+class bytebank extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: TranferList(),
-        appBar: AppBar(
-          title: const Text('Transfer'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-        ),
+        body: FormularyTransfer(),
       ),
-    ));
+    );
+  }
+}
+
+class FormularyTransfer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Transfer'),
+      ),
+    );
+  }
+}
 
 class TranferList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      TransferItem(Transfer(100.50, 1000)),
-      TransferItem(Transfer(150.90, 1000)),
-      TransferItem(Transfer(10.05, 1000)),
-    ]);
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          TransferItem(Transfer(100.50, 00444 - 5)),
+          TransferItem(Transfer(150.90, 00464 - 5)),
+          TransferItem(Transfer(10.05, 00449 - 2)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: const Text('Transfer'),
+      ),
+    );
   }
 }
 
@@ -34,8 +56,8 @@ class TransferItem extends StatelessWidget {
     return Card(
         child: ListTile(
       leading: const Icon(Icons.monetization_on),
-      title: Text("Value:" + _transfer.value.toString()),
-      subtitle: Text("Account number:" + _transfer.accountNumber.toString()),
+      title: Text("Value: \$" + _transfer.value.toString()),
+      subtitle: Text("Account number " + _transfer.accountNumber.toString()),
     ));
   }
 }
